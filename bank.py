b052162add38f5659f378f93a99b97228b5a79fc
petraelgemyr <petra.elgemyr@gmail.com>
2024-07-09 19:00:38 +0200

    first commit

---
+++ bank.py
@@ -0,0 +1,16 @@
+"""
+Skriv ett program som beräknar hur mycket pengar man får på ett bankkonto.
+
+Om man sätter in 100 000kr (hundratusen kronor) och låter pengarna stå inne under 5 år.
+
+När man kör programmet frågar det efter räntesatsen, som anges i procent t.ex. 3.5.
+
+Som resultat visar programmet en tabell där man kan se hur mycket kapitalet har växt efter varje år. 
+
+"""
+
+bankAccount = 100000
+years = 5
+yearlyInterest =float(input("Ange räntesatsen i procent, ex. 2.6:"))
+
+print(yearlyInterest)
